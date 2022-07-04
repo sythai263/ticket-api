ba13870fc3fdc1192e301e3314928134307793cc
Thái Lê Sỹ <sythai263@gmail.com>
2022-07-04 10:25:23 +0700

    chore: create repo

--- src/modules/ticket/repositories/index.ts
+++ src/modules/ticket/repositories/index.ts
@@ -1,4 +1,12 @@
+export { AttendeeRepository } from './attendee.repo';
+export { DetailOrderRepository } from './detailOrder.repo';
+export { DiscountRepository } from './discount.repo';
+export { InvoiceRepository } from './invoice.repo';
 export { ProductRepository } from './product.repo';
 export { ProgramRepository } from './program.repo';
+export { ProgramItemRepository } from './programItem.repo';
+export { PurchaseRepository } from './purchase.repo';
+export { ReviewProductRepository } from './reviewProduct.repo';
+export { ReviewProgramRepository } from './reviewProgram.repo';
 export { UserRepository } from './user.repo';
 
